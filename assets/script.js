f7606b0d9810a59ce3a99c02c0c07ebaa23ef070
Luke <121516961+Afrozez@users.noreply.github.com>
2023-05-03 12:32:37 -0700

    initial JavaScript added to display textContent into p tags

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+let currentCityTemp = 25;
+
+const currentTempDisplay = document.getElementById('current-temp');
+
+let currentTemp = `Temp: ${currentCityTemp}°F`;
+
+currentTempDisplay.textContent = currentTemp;
+
+
+
+let currentCityWinds = 8.43;
+
+const currentWindsDisplay = document.getElementById('current-winds');
+
+let currentWinds = `Wind: ${currentCityWinds} MPH`;
+
+currentWindsDisplay.textContent = currentWinds;
+
+
+let currentCityHumidity = 44;
+
+const currentHumidityDisplay = document.getElementById('current-humidity');
+
+let currentHumidity = `Temp: ${currentCityHumidity} %`;
+
+currentHumidityDisplay.textContent = currentHumidity;
