8faf4c2ce6e054f5c87066d674546cf8398a6b2f
Luke <121516961+Afrozez@users.noreply.github.com>
2023-05-04 21:57:03 -0700

    added a display:flex value to my main content flexbox and added a flex-grow property to the current city forecast

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,20 @@
 let currentCity = 'Atlanta'
 let currentDate = '(9/13/2022)';
+var weatherAPIKey = "ccd960f728362c855d666700bf7fb5df";
+var city;
 
 currentCityDisplay = document.getElementById('current-city-conditions');
-
 currentCityDisplay.textContent = `${currentCity} ` + `${currentDate}`;
 
 
+userInputArea = document.getElementById('inputValue');
+searchButton = document.querySelector('.search-button');
+console.log(searchButton);
+
+searchButton.addEventlistener("click", captureUserInput);
+
+lol = fetch();
+console.log();
 
 let currentCityTemp = 25;
 
